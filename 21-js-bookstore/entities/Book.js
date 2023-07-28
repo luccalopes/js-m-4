11fb53ee1589490d730e63f6e92752d64a3923fa
luccalopes <110003708+luccalopes@users.noreply.github.com>
2023-07-28 12:39:13 -0300

    28/07

--- 21-js-bookstore/entities/Book.js
+++ 21-js-bookstore/entities/Book.js
@@ -1,10 +1,11 @@
 const Product = require("./Product");
 
 module.exports = class Book extends Product{
-    constructor(title, synopsis, genre, author, description, price, inStock = 0) {
+    constructor(title, synopsis, genre, pages, author, description, price, inStock) {
         super(`'Livro ${title}`, description, price, inStock)
-        this.title = title
+        
         this.synopsis = synopsis
+        this.pages = pages
         this.genre = genre
         this.author = author
     }
